3638dfce1d78e479dbf13351db7cafbb7619a10d
Dorian Mutti <cloudyavi3@gmail.com>
2024-05-14 13:28:27 -0400

    - user input

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String selection;
+
         String[] authors = {"George RR Martin", "J.R.R. Tolkein", "Aurthur C. Clarke",
         "George Orwell", "Stephen King", "Peter Suskind", "Ernest Hemingway", 
         "J.K. Rowling", "Kurt Vonnegut", "William Shakespear", "Harper Lee", "Charles Dickens", 
@@ -25,7 +29,10 @@ public class Client {
             "The Wit and Wisdom of Tyrion Lannister", "Ice Dragon", "In the House of the Worm", 
             "A Knight of the Seven Kingdoms", "A Dance With Dragons: Part 2 After the Feast (A Song of Ice and Fire, Book 5)", "Songs of Love and Death", 
             "With Morning Comes Mistfall", "Songs of the Dying Earth", "Dark Visions", 
-            "Aces Abroad", "A Storm of Swords: Part 1 Steel and Snow (A Song of Ice and Fire, Book 3) "}
+            "Aces Abroad", "A Storm of Swords: Part 1 Steel and Snow (A Song of Ice and Fire, Book 3)", "The Second Kind of Loneliness"}
         };
+        System.out.println("View All Authors");
+        System.out.println("View Specific Author");
+        selection = scanner.nextLine();
     }
 }
